54c30dde729b2bde18f774e119509519e2f6d7de
RickBm400 <rjbm29@gmail.com>
2024-07-25 14:54:22 -0500

    feat: cards component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import "./landing.sass";
+import Grid from "@mui/material/Unstable_Grid2";
+import ContentCards from "@/components/ContentCards";
 
 export default function Home() {
   return (
@@ -20,7 +22,25 @@ export default function Home() {
         </div>
       </section>
       <section id="about" className="flex items-center">
-        olamundo
+        <div className="about__description flex flex-col">
+          <h1 className="mb-auto">Olamundos</h1>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sit
+            praesentium facere soluta possimus minus ut? Et quibusdam ratione,
+            dignissimos sequi pariatur dolor vitae nemo similique delectus,
+            adipisci molestias consequuntur!
+          </p>
+        </div>
+        <div className="about__scroller">
+          <Grid
+            container
+            className="h-[100%] w-[100%] flex items-stretch justify-space-around"
+          >
+            {[1, 2, 3].map((numer) => (
+              <ContentCards key={numer}></ContentCards>
+            ))}
+          </Grid>
+        </div>
       </section>
     </main>
   );
