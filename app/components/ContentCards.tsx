cd699418827a06c77029690d1df1be0f1a767dc5
RickBm400 <rjbm29@gmail.com>
2024-07-25 15:02:35 -0500

    update content card

---
+++ app/components/ContentCards.tsx
@@ -0,0 +1,8 @@
+import { Card, CardContent } from "@mui/material";
+export default function ContentCard() {
+  return (
+    <Card sx={{ height: "100%" }}>
+      <CardContent>olamundos</CardContent>
+    </Card>
+  );
+}
