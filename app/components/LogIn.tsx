75544f8ddbb5984cb81c7f9eb8f3f472f094c30a
RickBm400 <rjbm29@gmail.com>
2024-07-26 21:52:35 -0500

    style: about dimentions

--- app/components/LogIn.tsx
+++ app/components/LogIn.tsx
@@ -2,11 +2,7 @@ import { Card } from "@mui/material";
 
 export default function LoginCards() {
   return (
-    <Card
-      elevation={0}
-      sx={{ bgcolor: "red" }}
-      className="flex-1 rounded-[12px]"
-    >
+    <Card elevation={0} className="flex-1 rounded-[12px] p-6 bg-[#c4daec]">
       Olamunfos
     </Card>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -22,8 +22,8 @@ export default function Home() {
           <h1>COWORKING SPACE</h1>
         </div>
       </section>
-      <section id="about" className="flex items-center">
-        <div className="about__description flex flex-col mr-10">
+      <section id="about" className="flex space-x-16 ">
+        <div className="about__description flex flex-col ">
           <h2 className="mb-auto">Olamundos</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sit
@@ -32,22 +32,17 @@ export default function Home() {
             adipisci molestias consequuntur!
           </p>
         </div>
-        <div className="about__scroller">
-          <Grid className="h-[100%] w-[100%] flex space-x-6 justify-space-around">
-            {[1, 2, 3].map((numer) => (
-              <ContentCards
-                key={numer}
-                classItems={"flex-1 rounded-[12px]"}
-              ></ContentCards>
-            ))}
-          </Grid>
+        <div className="about__scroller flex space-x-6 items-end ">
+          {[1, 2, 3].map((numer) => (
+            <ContentCards
+              key={numer}
+              classItems={"flex-1 rounded-[12px] h-[80%]"}
+            ></ContentCards>
+          ))}
         </div>
       </section>
       <section id="logins" className="flex justify-center items-center">
-        {/* <div className="w-[40%]">
-          <span>Items title</span>
-        </div> */}
-        <div className="logins__cards flex w-[60%] space-x-6 p-4">
+        <div className="logins__cards flex w-full  space-x-6 p-4 mb-16">
           {[1, 2].map((cardId) => (
             <LoginCards key={cardId} />
           ))}
