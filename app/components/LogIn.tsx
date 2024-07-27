2822a487a6760cd800931b6331b9d1aec5ec748b
RickBm400 <rjbm29@gmail.com>
2024-07-26 21:15:03 -0500

    feat: login cards section

---
+++ app/components/LogIn.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@mui/material";
+
+export default function LoginCards() {
+  return (
+    <>
+      <Card elevation={0} sx={{ bgcolor: "red" }}>
+        Olamunfos
+      </Card>
+    </>
+  );
+}
