434dd18f886ec550f43c945296bb0bf26375be79
RickBm400 <rjbm29@gmail.com>
2024-08-05 18:09:29 -0500

    new fonts -michroma & inter-

---
+++ app/utils/font.ts
@@ -0,0 +1,14 @@
+import { Michroma, Inter } from "next/font/google";
+import { NextFont } from "next/dist/compiled/@next/font";
+
+export const michroma: NextFont = Michroma({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+});
+
+export const inter: NextFont = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+});
