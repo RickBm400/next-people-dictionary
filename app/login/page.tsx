f6e1e6f2e1206a916f6e2defa65af46481779b94
RickBm400 <rjbm29@gmail.com>
2024-08-11 02:14:38 -0500

    feat: login css complements

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { Box } from "@mui/material";
 import React, { useState } from "react";
+import { Box, Grid, Button } from "@mui/material";
+import { michroma, inter } from "@/utils/font";
 import TexFieldComps from "globalComponents/textField";
 import "./login.sass";
 
@@ -19,23 +20,51 @@ export default function Login() {
   return (
     <main id="login" className="flex h-[100vh]">
       <section className="login__info w-[60%] h-screen grid ">
-        <div className="login__info-background">olas</div>
+        <div className="login__info-background">
+          <div className="login__info-background-title">
+            <h1
+              className={`${michroma.className}`}
+              style={{ fontWeight: "700" }}
+            >
+              Login
+            </h1>
+            <div className="corner-1"></div>
+            <div className="corner-2"></div>
+          </div>
+        </div>
       </section>
       <section className="login__form w-[40%] h-screen grid  bg-white">
-        <div className="login__form-div">
+        <div className="login__form-div flex items-center justify-center">
           <Box component="form" noValidate autoComplete="off">
-            <TexFieldComps
-              value={email}
-              label="email"
-              type="email"
-              handler={handleEmail}
-            ></TexFieldComps>
-            <TexFieldComps
-              value={password}
-              label="Password"
-              type="password"
-              handler={handlePassword}
-            ></TexFieldComps>
+            <Grid container spacing={4}>
+              <Grid item xs={12}>
+                <TexFieldComps
+                  value={email}
+                  label="email"
+                  type="email"
+                  handler={handleEmail}
+                ></TexFieldComps>
+              </Grid>
+              <Grid item xs={12}>
+                <TexFieldComps
+                  value={password}
+                  label="Password"
+                  type="password"
+                  handler={handlePassword}
+                ></TexFieldComps>
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  disableElevation
+                  className={`${inter.className} capitalize font-[700]`}
+                  style={{ fontWeight: "800" }}
+                >
+                  Login
+                </Button>
+              </Grid>
+            </Grid>
           </Box>
         </div>
       </section>
